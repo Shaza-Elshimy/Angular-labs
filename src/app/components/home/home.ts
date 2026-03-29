e80b4b89a2efe58939dba6eeb6e3445bf389ced8
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-03-30 00:12:31 +0200

    move student selector

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { NgForOf } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
-import { IStudent } from '../../models/istudent';
 import { Student } from "../student/student";
 import { Courses } from "../courses/courses";
 @Component({
   selector: 'app-home',
-  imports: [Student, Courses],
+  imports: [ Courses],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
